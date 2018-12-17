3680108fb0595343476d689bad8e911f6ac203d4
anisonglopez <poomwattanaklang@gmail.com>
2018-12-17 10:34:05 +0700

    ajax load datatable

--- attentran.php
+++ attentran.php
@@ -7,15 +7,7 @@ if($_SESSION['UserID'] == "")
 }
 ?>
 <?php include("includes/header.php"); ?>
-<?php 
-$sql = "SELECT tt04_attendance.* , tm03_employee.EmplTName, tm03_employee.EmplType,tm02_attncode.AttnTDesc
-FROM tt04_attendance
-JOIN tm03_employee ON tt04_attendance.EmplCode=tm03_employee.EmplCode
-JOIN tm02_attncode ON tt04_attendance.AttnCode=tm02_attncode.AttnCode";
-$DATA = mysqli_query($conn, $sql);
 
-
-?>
 
         <div class="row">
 
@@ -23,8 +15,6 @@ $DATA = mysqli_query($conn, $sql);
             <div class="col-md-12">
                 <h1>ประวัติการลงเวลา</h1>
             <hr>
-            
-          
             <div class="row">
                 <div class="col-lg-12">
                     <div class="panel panel-default">
@@ -35,72 +25,11 @@ $DATA = mysqli_query($conn, $sql);
                         </div>
                         <!-- /.panel-heading -->
                         <div class="panel-body">
-                            <table width="100%" class="table table-striped table-bordered table-hover" id="dataTables-example">
-                                <thead>
-                                    <tr>
-                                    <th scope="col">วันที่ลา</th>
-                                    <th scope="col">รหัสพนักงาน</th>
-                                    <th scope="col">ชื่อพนักงาน</th>
-                                    <th scope="col">ประเภทพนักงาน</th>
-                                    <th scope="col">ประเภทการลา</th>
-                                    <th scope="col">จำนวน(ชม.)</th>
-                                    <th scope="col">หักเงิน</th>
-                                    <th scope="col" style="text-align: center;">Action</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                <?php
-  while ($rows = mysqli_fetch_array($DATA)) {
-    $id = $rows['auto_increment'];
-    
-    $AttnDate = $rows['AttnDate'];
-    $AttnDate = date('d-m-Y', strtotime($AttnDate));
-    $Sort_Attndate = date('Ymd', strtotime($AttnDate));
-    $EmplCode = $rows['EmplCode'];
-    $EmplTName = $rows['EmplTName'];
-    $EmplType = $rows['EmplType'];
-    $AttnTDesc = $rows['AttnTDesc'];
-    $Hours = $rows['Hours'];
-    $Ded_Flag = $rows['Ded_Flag'];
-    // $DeductTDesc = $rows['DeductTDesc'];
-    // $Remark = $rows['Remark'];
-    // $Amount = $rows['Amount'];
-    // $TaxCalFlag = $rows['TaxCalFlag'];
-    if( $Ded_Flag == "1"){
-      $Ded_Flag = '<span style="color :green; font-weight: 800;" class="glyphicon glyphicon-ok" aria-hidden="true"></span>';
-    }
-    else{
-      $Ded_Flag = '<span style="color :red; font-weight: 800;" class="glyphicon glyphicon-remove" aria-hidden="true"></span>';
-    }
-    if( $EmplType == "D"){
-        $EmplType = "รายวัน";
-      }
-      else{
-        $EmplType = "รายเดือน";
-      }
-  ?>
-                                <tr>
-                                <td><span class='hide'><?php echo $Sort_Attndate; ?></span><?php echo $AttnDate; ?></td>
-                                <td style="text-align: center;"><?php echo $EmplCode; ?></td>
-                                <td><?php echo $EmplTName; ?></td>
-                                <td><?php echo $EmplType; ?></td>
-                                <td><?php echo $AttnTDesc; ?></td>
-                                <td><?php echo $Hours ; ?></td>
-                                <td><?php echo $Ded_Flag; ?></td>
-                                <td>
-                                <center>
-                                <a href="attentran_change.php?id=<?php echo $id?>">
-                                <button  class="btn btn-warning"><span class="glyphicon glyphicon-edit" aria-hidden="true"></span></button>
-                                </a>
-                                <a href="JavaScript:if(confirm('Confirm Delete?')==true){window.location='attentran_delete.php?id=<?php echo $id?>';}">
-                            <button  class="btn btn-danger delete_id"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></button>
-                            </a>
-                            </center>
-                            </td>
-                            </tr>
-<?php } ?>                          
-                                </tbody>
-                            </table>
+                        <div id="loading-image" style="display:none; text-align: center;" >
+                            <img src="img/loading_gif/flat-progress-bar-stripe-loader.gif" /> กรุณารอซักครู่.... ระบบกำลังโหลดข้อมูล
+                        </div>           
+                        <div id="table_detail"></div>
+                           
                             <!-- /.table-responsive -->
          </div>
 
@@ -109,11 +38,21 @@ $DATA = mysqli_query($conn, $sql);
     <!-- Blog Sidebar Widgets Column -->
 <?php include("includes/footer.php"); ?>
 <script>
-    $(document).ready(function() {
-        $('#dataTables-example').DataTable({
-            responsive: true ,
-            "order": [[ 0, "desc" ]]
-            
-        });
-    });
-    </script>
+$(document).ready(function() {
+    $.ajax({  
+            url:"attentran_ajax_datatable.php",  
+                method:"post",  
+                data:{},  
+                    beforeSend: function() {
+                        $("#loading-image").show();
+                    },
+                success:function(data){          
+                    $('#table_detail').html(data);  
+                    $("#loading-image").hide();
+                },
+                error: function (jqXHR, exception) {
+                    document.write(exception);
+                }
+        });  
+ });
+</script>
